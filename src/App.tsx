e76f874abb50795397bba7fd35967e59c5a7d7e0
TroianDmytro <146668649+TroianDmytro@users.noreply.github.com>
2024-08-20 19:00:40 +0300

    #Update

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,36 @@
 import React from 'react';
 import './App.css';
-import axios from 'axios';
+// import axios from 'axios';
 import Content from './components/Content/Content';
 
 // const myFetch= async ()=>{
 //   await axios.get("https://localhost:44319/Armor").then(res=>console.log(res.data));
 // }
-const myFetch= async ()=>{
-  await axios.get("http://troyankh62-001-site1.ftempurl.com/Tank",{
-    headers: {Authorization: `Basic MTExODk5MDA6NjAtZGF5ZnJlZXRyaWFs`,
-    'Content-Type': 'application/json',
-     Accept: "*/*",
-     'Access-Control-Allow-Origin':'*'}
-  }
+// const myFetch= async ()=>{
+//   await axios.get("http://troyankh62-001-site1.ftempurl.com/Tank/4",{
     
-  ).then(res=>console.log(res.data));
+//     headers: {Authorization: `Basic ` + btoa('11189900'+':'+'60-dayfreetrial'),
+//       withCredentials: true,
+//       crossorigin: true,
+//       mode: 'no-cors',     
+//       ContentType: 'application/json',
+//      AccessControlAllowOrigin:'*'},
+     
+//   }
+    
+//   ).then(res=>console.log(res.data));
 
+// }
+
+const myFetch = async()=>{
+  fetch("http://troyankh62-001-site1.ftempurl.com/Tank",{
+    method:'get',
+    headers:new Headers({
+      'Authorization':`Basic ` + btoa('11189900:60-dayfreetrial')
+    })
+  }).then(res=>res).then(data => console.log(data));
 }
+
 function App() {
   
   return (
